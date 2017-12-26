ef10b44572cb2541934609f7779dab9ee023aca7
Jose <jmgallego@alea-soluciones.com>
2017-12-26 12:50:06 +0100

    Imported my first component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,14 +2,22 @@ import React, { Component } from 'react';
 import logo from '../images/logo.svg';
 import './App.css';
 
+
+// Importing components
+
+import Header from '../Header/Header';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
+        <div className="App">
+            <div>
+                <Header/>
+            </div>
+
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
-        </header>
+
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/components/Header/Header.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from '../images/logo.svg';
+import '../App/App.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="Header">
+        <header className="Logo">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </header>
+        <p className="App-intro">
+            Ok, this is the header component
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Header;
